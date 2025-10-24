a54a63d0a182582c58accd1d9aeefa90092d472d
rodbezbackendshah-waz <rodbezbackend.shah@gmail.com>
2025-10-25 02:12:09 +0530

    newchnagePR

---
+++ src/app/AboutMe/page.js
@@ -0,0 +1,13 @@
+// // /main/AboutMe/page.js
+// import React from "react";
+// import AboutMe from "../component/AboutMe";
+
+// const page = () => {
+//   return (
+//     <div>
+//      <AboutMe />
+//     </div>
+//   );
+// };
+
+// export default page; // ✅ default export must be a React component
