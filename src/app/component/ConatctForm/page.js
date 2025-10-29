e2d059cee70f73cd692d661eb64b19c98e42722b
rodbezbackendshah-waz <rodbezbackend.shah@gmail.com>
2025-10-30 02:58:19 +0530

    fix: replaced deprecated emailjs-com with @emailjs/browser

--- src/app/component/ConatctForm/page.js
+++ src/app/component/ConatctForm/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
+
 
 export default function ContactForm() {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
